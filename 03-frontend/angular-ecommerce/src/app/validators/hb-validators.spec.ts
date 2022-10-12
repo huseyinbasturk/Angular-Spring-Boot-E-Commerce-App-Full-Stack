223686e4db500c0b16a5e24013b1be2b56e4f8ac
huseyinbasturk <hb.basturk41@gmail.com>
2022-10-12 16:25:18 +0200

    checkout form validation was added

---
+++ 03-frontend/angular-ecommerce/src/app/validators/hb-validators.spec.ts
@@ -0,0 +1,7 @@
+import { HbValidators } from './hb-validators';
+
+describe('HbValidators', () => {
+  it('should create an instance', () => {
+    expect(new HbValidators()).toBeTruthy();
+  });
+});
