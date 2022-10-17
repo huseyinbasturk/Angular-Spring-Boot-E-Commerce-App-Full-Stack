f84b6cd1c9e54313d3fff6f41d2402bc35cef821
huseyinbasturk <hb.basturk41@gmail.com>
2022-10-17 12:30:47 +0200

    secure communication with https/Angular Frontend

--- 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
+++ 03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -3,7 +3,8 @@ export default {
     oidc: {
         clientId: '0oa6ufbkua553Za2q5d7',
         issuer: 'https://dev-08980789.okta.com/oauth2/default',
-        redirectUri: 'http://localhost:4200/login/callback',
+        //redirectUri: 'http://localhost:4200/login/callback',
+        redirectUri: 'https://localhost:4200/login/callback',
         scopes: ['openid', 'profile','email']
     }
 }
